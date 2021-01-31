afce5710e188f2f6925690e44d9d3a17ad53ecb0
David Tran <davtran303@gmail.com>
2021-01-30 18:26:54 -0700

    made date work

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var textBlock = ""
+var saveButton; // var used to use id = saveBtn
+
+
+document.getElementById("text").innerHTML=moment().format('MMMM Do YYYY, h:mm:ss a');
+document.getElementById("text1").innerHTML=moment().format('MMMM Do YYYY, h:mm:ss a');
+document.getElementById("text2").innerHTML=moment().format('moment().calendar();');
+document.getElementById("text3").innerHTML=moment().format('MMMM Do YYYY, h:mm:ss a');
+document.getElementById("text4").innerHTML=moment().format('MMMM Do YYYY, h:mm:ss a');
+
+  
