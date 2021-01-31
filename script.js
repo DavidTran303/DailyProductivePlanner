9f1c310f8b680f8cd2b9b6938e4111d95e7239b7
David Tran <davtran303@gmail.com>
2021-01-30 18:37:55 -0700

    made time display

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ var textBlock = ""
 var saveButton; // var used to use id = saveBtn
 
 
-document.getElementById("text").innerHTML=moment().format('MMMM Do YYYY, h:mm:ss a');
-document.getElementById("text1").innerHTML=moment().format('MMMM Do YYYY, h:mm:ss a');
-document.getElementById("text2").innerHTML=moment().format('moment().calendar();');
-document.getElementById("text3").innerHTML=moment().format('MMMM Do YYYY, h:mm:ss a');
-document.getElementById("text4").innerHTML=moment().format('MMMM Do YYYY, h:mm:ss a');
-
-  
+document.getElementById("text").innerHTML=moment().subtract(3, 'days').calendar();
+document.getElementById("text1").innerHTML=moment().subtract(1, 'days').calendar();
+document.getElementById("text2").innerHTML=moment().format('llll');
+document.getElementById("text3").innerHTML=moment().startOf('day').fromNow(); 
+document.getElementById("text4").innerHTML=moment().add(1, 'days').calendar();
